1fbe468344bbe8fadd973b549f072ba656acb640
Reketino <bjornevensk8@gmail.com>
2026-05-04 11:43:25 +0200

    Feat: setup clientwrapper and live data component

---
+++ src/components/LiveData.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Status from "./Status";
+import Oil from "./Brentcrude";
+import Events from "./Events";
+
+type Data = {
+    status: "YES" | "MAYBE" | "NO";
+    events: { title: string; url: string }[];
+    oil: { price: number; change: number };
+    updatedAt: string;
+};
+
+export default function LiveData({ initialData}: {initialData: Data }) {
+    const [data, setData] = useState(initialData);
+
+    useEffect(() => {
+        const interval = setInterval(async () => {
+            try {
+                const res = await fetch("/api/status");
+                const fresh = await res.json();
+
+                setData(fresh);
+            } catch (err) {
+                console.error("Live update has faulted", err);
+            }
+            }, 60000);
+
+            return () => clearInterval(interval);
+        },[]);
+
+        return (
+            <>
+            <section className="flex flex-col items-center">
+                <Status status={data.status} />
+                <Oil price={data.oil.price} change={data.oil.change} />
+            </section>
+
+            <Events events={data.events} updatedAt={data.updatedAt} />
+            </>
+        );
+    }
