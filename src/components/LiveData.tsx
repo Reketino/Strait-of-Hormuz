e6ff344e6b734cab67d930a625ebc994d1e1f90e
Reketino <bjornevensk8@gmail.com>
2026-05-07 00:09:56 +0200

    Style: formatting

--- src/components/LiveData.tsx
+++ src/components/LiveData.tsx
@@ -6,45 +6,45 @@ import Oil from "./Brentcrude";
 import Events from "./Events";
 
 type Data = {
-    status: "YES" | "MAYBE" | "NO";
-    events: { title: string; url: string }[];
-    oil: { price: number; change: number };
-    updatedAt: string;
+  status: "YES" | "MAYBE" | "NO";
+  events: { title: string; url: string }[];
+  oil: { price: number; change: number };
+  updatedAt: string;
 };
 
-export default function LiveData({ initialData}: {initialData: Data }) {
-    const [data, setData] = useState(initialData);
+export default function LiveData({ initialData }: { initialData: Data }) {
+  const [data, setData] = useState(initialData);
 
-    useEffect(() => {
-        let interval: NodeJS.Timeout;
-            
-        const startPolling = () => {
-        interval = setInterval(async () => {
-            try {
-                if (document.hidden) return;
-                const res = await fetch("/api/status");
-                const fresh = await res.json();
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
 
-                setData(fresh);
-            } catch (err) {
-                console.error("Live update has failed", err);
-            }
-            },  60000);
-        };
+    const startPolling = () => {
+      interval = setInterval(async () => {
+        try {
+          if (document.hidden) return;
+          const res = await fetch("/api/status");
+          const fresh = await res.json();
 
-        startPolling();
+          setData(fresh);
+        } catch (err) {
+          console.error("Live update has failed", err);
+        }
+      }, 60000);
+    };
 
-        return () => clearInterval(interval);
-        },[]);
+    startPolling();
 
-        return (
-            <>
-            <section className="flex flex-col items-center">
-                <Status status={data.status} />
-                <Oil price={data.oil.price} change={data.oil.change} />
-            </section>
+    return () => clearInterval(interval);
+  }, []);
 
-            <Events events={data.events} updatedAt={data.updatedAt} />
-            </>
-        );
-    }
+  return (
+    <>
+      <section className="flex flex-col items-center">
+        <Status status={data.status} />
+        <Oil price={data.oil.price} change={data.oil.change} />
+      </section>
+
+      <Events events={data.events} updatedAt={data.updatedAt} />
+    </>
+  );
+}
