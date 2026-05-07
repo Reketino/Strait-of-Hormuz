f93f3f659fdeb924cde3f18f04928d6a0bf568db
Reketino <bjornevensk8@gmail.com>
2026-05-08 00:18:17 +0200

    Style: formatting, UI, no logic change

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,18 +1,16 @@
 import { StraitStatus } from "@/types/status";
 
-export default function Status ({ status }: { status: StraitStatus }) {
-    const color =
+export default function Status({ status }: { status: StraitStatus }) {
+  const color =
     status === "YES"
-    ? "text-green-400"
-    : status === "MAYBE"
-    ? "text-yellow-400"
-    : "text-red-500";
+      ? "text-green-400"
+      : status === "MAYBE"
+        ? "text-yellow-400"
+        : "text-red-500";
 
-    return (
-        <main className="text-center">
-            <section className={`text-7xl font-extrabold ${color}`}>
-                {status}
-            </section>
-        </main>
-    );
+  return (
+    <main className="text-center">
+      <section className={`text-7xl font-extrabold ${color}`}>{status}</section>
+    </main>
+  );
 }
