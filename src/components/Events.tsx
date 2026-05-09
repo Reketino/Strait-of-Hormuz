6164580feea1d293fc00749f128bfb6be44e784b
Reketino <bjornevensk8@gmail.com>
2026-05-09 10:20:15 +0200

    Refactor: p in arrow to span

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -57,9 +57,9 @@ export default function Events({ events, updatedAt }: Props) {
             </span>
             </div>
 
-            <p className="text-gray-600 opacity-0 -translate-x-1 transition-all duration-300 group-hover:opacity-100 group-hover:translate-x-0 group-hover:text-white">
+            <span className="text-gray-600 opacity-0 -translate-x-1 transition-all duration-300 group-hover:opacity-100 group-hover:translate-x-0 group-hover:text-white">
               ↗️
-            </p>
+            </span>
           </a>
         ))}
       </ul>
