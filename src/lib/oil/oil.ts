82a7be628745c3b50ccd2097d427be7ba7f7024b
Reketino <bjornevensk8@gmail.com>
2026-05-09 10:38:33 +0200

    Chore: removed manual cache & in oil.ts & only using next cache

--- src/lib/oil/oil.ts
+++ src/lib/oil/oil.ts
@@ -4,17 +4,8 @@ export type OilData = {
   updatedAt: number;
 };
 
-let cache: OilData | null = null;
-let lastFetch = 0;
-
-const CACHE_TIME = 15 * 60 * 1000;
-
 export async function getOilPrice(): Promise<OilData> {
   const now = Date.now();
-
-  if (cache && now - lastFetch < CACHE_TIME) {
-    return cache;
-  }
 
   const res = await fetch(
     "https://api.api-ninjas.com/v1/commodityprice?name=brent_crude_oil",
@@ -34,14 +25,9 @@ export async function getOilPrice(): Promise<OilData> {
 
   const data = await res.json();
 
-  const result: OilData = {
+  return {
     price: data.price,
     change: data.change ?? 0,
     updatedAt: now,
   };
-
-  cache = result;
-  lastFetch = now;
-
-  return result;
 }
