8561eeaee3af89bbfc6fe8dd2cf67d4eba2f3254
Reketino <bjornevensk8@gmail.com>
2026-05-12 11:21:22 +0200

    Refactor: semantics in storypage

--- src/app/story/page.tsx
+++ src/app/story/page.tsx
@@ -1,17 +1,17 @@
 
 export default function StoryPage(){
     return(
-<main className="relative h-screen flex items-center justify-center overflow-hidden">
- <section className="absolute inset-0 bg-black">
+<section className="relative h-screen flex items-center justify-center overflow-hidden">
+ <header className="absolute inset-0 bg-black">
     <img
     src=""
     alt=""
     />
 
     <div className="" />
+</header>
+
+
 </section>
-
-
-</main>
 )
 }
