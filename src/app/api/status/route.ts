4d9a04124c3e89cdf2d0ff47741413ef6c77bf78
Reketino <bjornevensk8@gmail.com>
2026-05-15 09:56:40 +0200

    Refactor: events in route, to hopefully start updating oil price

--- src/app/api/status/route.ts
+++ src/app/api/status/route.ts
@@ -1,10 +1,11 @@
 import { fetchNews } from "@/lib/news/news";
 import { manualStatus } from "@/lib/manualStatus";
+import { getOilPrice } from "@/lib/oil/oil";
 
 export const runtime = "edge";
 
 export async function GET() {
-    const events = await fetchNews();
+    const [events, oil] = await Promise.all([fetchNews(), getOilPrice()]);
 
     return Response.json({
         status: manualStatus,
