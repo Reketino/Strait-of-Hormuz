2a6ea45a30be8cd283faa27db38752837a6ccaf7
Reketino <bjornevensk8@gmail.com>
2026-05-19 00:03:50 +0200

    Feat: setup text in intro

--- src/components/story/Intro.tsx
+++ src/components/story/Intro.tsx
@@ -2,6 +2,9 @@ export default function Intro() {
     return (
         <section className="bg-black py-32">
         <article className="mx-auto max-w-6xl px-6">
+            <p className="">
+                
+            </p>
             
         </article>
             
