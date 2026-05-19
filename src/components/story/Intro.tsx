b9136cfd5e09b6e90da678d81d634f707412258f
Reketino <bjornevensk8@gmail.com>
2026-05-20 00:05:03 +0200

    Style: Added style to text

--- src/components/story/Intro.tsx
+++ src/components/story/Intro.tsx
@@ -2,7 +2,7 @@ export default function Intro() {
     return (
         <section className="bg-black py-32">
         <article className="mx-auto max-w-6xl px-6">
-            <p className="">
+            <p className="mb-4 text-sm uppercase tracking-[0.3em] text-gray-500">
                 
             </p>
             
