0dad9e2c09234142f03cd104129d3ce9aee65e90
Reketino <bjornevensk8@gmail.com>
2026-06-02 00:03:56 +0200

    Feat: setup article section in chokepoint component

--- src/components/story/Chokepoint.tsx
+++ src/components/story/Chokepoint.tsx
@@ -1,7 +1,9 @@
 export default function Chokepoint() {
     return (
         <section className="py-32 bg-neutral-950">
+            <article className="mx-auto max-w-6xl px-6 grid gap-16 lg:grid-cols-2">
 
+            </article>
         </section>
     )
 }
