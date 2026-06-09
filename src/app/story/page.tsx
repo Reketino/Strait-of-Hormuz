6c881e02745cde81a9f57968cc12460dd4b64863
Reketino <bjornevensk8@gmail.com>
2026-06-10 00:01:01 +0200

    feat: imported chokepoint into storypage

--- src/app/story/page.tsx
+++ src/app/story/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/story/Hero"
 import Intro from "@/components/story/Intro";
+import Chokepoint from "@/components/story/Chokepoint";
 
 export default function StoryPage(){
     return(
