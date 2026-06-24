a0244be54d37d0823bb1d4c10c0af88bf3d38ee1
Reketino <bjornevensk8@gmail.com>
2026-06-25 00:05:05 +0200

    feat: setup figure w/ styling

--- src/components/story/MapSection.tsx
+++ src/components/story/MapSection.tsx
@@ -1,6 +1,9 @@
 export default function MapSection() {
     return(
-        <section className="">
+        <section className="relative min-h-screen overflow-hidden">
+            <figure className="absolute inset-0">
+                
+            </figure>
             
         </section>
     )
