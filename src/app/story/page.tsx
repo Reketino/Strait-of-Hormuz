6d0556cbef1352c7d846223249e6c2167df0e12a
Reketino <bjornevensk8@gmail.com>
2026-08-21 00:00:46 +0200

    feat: add TimelineSection to StoryPage and update MapSection image path

--- src/app/story/page.tsx
+++ src/app/story/page.tsx
@@ -4,6 +4,7 @@ import Chokepoint from "@/components/story/Chokepoint";
 import OilSection from "@/components/story/OilSection";
 import MapSection from "@/components/story/MapSection";
 import TrafficSection from "@/components/story/TrafficSection";
+import TimelineSection from "@/components/story/TimelineSection";
 
 export default function StoryPage() {
   return (
@@ -14,6 +15,7 @@ export default function StoryPage() {
       <OilSection />
       <MapSection />
       <TrafficSection />
+      <TimelineSection />
     </main>
   );
 }
