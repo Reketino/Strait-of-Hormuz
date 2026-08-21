fdbabf7c830ad131bc38c4feb5d96d24e66e61df
Reketino <bjornevensk8@gmail.com>
2026-08-22 00:00:55 +0200

    feat: add MilitarySection component with basic structure

---
+++ src/components/story/MilitarySection.tsx
@@ -0,0 +1,7 @@
+export default function MilitarySection() {
+    return (
+        <section className="relative flex min-h-screen items-center overflow-hidden bg-black">
+            
+        </section>
+    )
+}
